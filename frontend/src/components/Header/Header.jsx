1d941a5087e16316240f426ec75a8aa292029e16
perceptkraft <141613035+perceptkraft@users.noreply.github.com>
2023-08-14 17:30:25 +0530

    Header Modification
    Modification as per sir

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Fade from "react-reveal/Fade";
 import Logo from "../../images/sliderlogo.png";
 import { FaSearch } from 'react-icons/fa'
+import Modal from 'react-bootstrap/Modal'
 
 const Header = () => {
-    return (
-
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
+    return (<>
         <Fade top>
             <div className='headerClass sticky-top shadow bg-dark'>
-                <Navbar expand="lg"  style={{ textTransform: 'uppercase', fontFamily: 'Roboto', fontWeight: '600' }}>
+                <Navbar expand="lg" style={{ textTransform: 'uppercase', fontFamily: 'Roboto', fontWeight: '400', letterSpacing: '2px', }}>
                     <Container>
-                        <Navbar.Brand href="#"><img src={Logo} alt='' width="40px" height="40px"  /></Navbar.Brand>
+                        <Navbar.Brand href="#"><img src={Logo} alt='' width="100px" height="80px" /></Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="m-auto d-flex justify-content-center">
@@ -36,17 +39,28 @@ const Header = () => {
                             </Nav>
                             <Nav>
                                 <form class="form-inline my-2 my-lg-0">
-                                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                                    <button class="btn btn-outline-light my-2 my-sm-0 rounded-5" type="submit">
+                                    <button class="btn btn-outline-light my-2 my-sm-0 rounded-5" type="submit" onClick={handleShow}>
                                         <FaSearch />
                                     </button>
                                 </form>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
+                 
                 </Navbar>
             </div>
         </Fade>
+        <div>
+               <Modal show={show} onHide={handleClose}>
+                        <Modal.Header>
+                            <Modal.Title>Modal heading</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            Woohoo, you're reading this text in a modal!
+                        </Modal.Body>
+                    </Modal>
+        </div>
+        </>
     )
 }
 
